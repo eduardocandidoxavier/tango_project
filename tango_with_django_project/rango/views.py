9a0f3bfbee318d3e6e900066a24ed192ceacac76
Eduardo Xavier <eduardo.xavier@gmail.com>
2017-10-24 10:02:45 -0200

    With error

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,4 +6,5 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse('Rango says, here is the about page! <br/> <a href="/rango/">Index</a>')
+    context_dict = {'author': 'Eduardo C Xavier'}
+    return render(request, 'rango/about.html', context_dict)
