8256428b456af9d9be15d88545af1687a455792a
Eduardo Xavier <eduardo.xavier@gmail.com>
2017-11-23 11:37:51 -0200

    Included link 'show all Categories' that lists with pagination all categories in the system. Also changed the populate_rango to include several users at once, and pages and categories.

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -4,39 +4,57 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.setti
 
 import django
 django.setup()
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
+from django.contrib.auth.models import User
 #This lines above makes the Django environment available so that we can use the models
 
 def populate():
-    pages1 = [
-        {'title': 'Eduardo C. Xavier',
+    users = [  {'email': 'eduardo@ic.unicamp.br'+str(i),
+                'password': 'teste'+str(i),
+                'website': 'www.example.com'
+                }  for i in range(100)
+            ]
+    pages_professores = [
+        {'title': 'Eduardo C. Xavier'+str(i),
         'url': 'http://www.ic.unicamp.br/~eduardo',
-         'views': 2},
+         'views': i} for i in range(100)
 
-        {'title': 'Zanoni Dias',
-         'url': 'http://www.ic.unicamp.br/~zanoni',
-         'views': 3},
     ]
 
-    pages2 = [
-        {'title': 'Folha de São Paulo',
+
+    pages_professores2 = [
+        {'title': 'Eduardo C. Xavier2'+str(i),
+        'url': 'http://www.ic.unicamp.br/~eduardo',
+         'views': i} for i in range(100)
+
+    ]
+
+    pages_jornais = [
+        {'title': 'Folha de São Paulo'+str(i),
          'url': 'http://www.folha.uol.com.br',
-         'views': 4},
-
-        {'title': 'Estado de São Paulo',
-         'url': 'http://www.estadao.com.br',
-         'views': 5},
+         'views': i} for i in range(100)
     ]
 
-    cats = { 'Professores IC': {'pages': pages1, 'views': 10, 'likes': 5},
-              'Jornais': {'pages': pages2, 'views': 10, 'likes': 5},
+    cats = { 'Professores IC': {'pages': pages_professores, 'views': 1000, 'likes': 500},
+              'Jornais': {'pages': pages_jornais, 'views': 10000, 'likes': 500},
              }
 
+    cats2 = {'Cat'+str(i): {'pages': pages_professores2, 'views': i, 'likes': i} for i in range(1,100)}
+
+
+    add_categories(cats)
+    add_categories(cats2)
+    add_users(users)
+
+def add_users(users):
+    for user in users:
+        add_user(user)
+
+def add_categories(cats):
     for cat in cats.keys():
         c = add_category(cat, cats[cat])
         for page in cats[cat]['pages']:
             add_page(c, page['title'],page['url'], page['views'])
-    print_categories_pages()
 
 def print_categories_pages():
     for c in Category.objects.all():
@@ -51,13 +69,35 @@ def add_category(cat, info):
     return c
 
 def add_page(category, title, url, views=0):
-    p = Page.objects.get_or_create(title=title)[0]
+    try:
+        p = Page.objects.get(title=title)
+    except Page.DoesNotExist:
+        p = Page()
+    p.title = title
     p.category = category
     p.url = url
     p.views = views
     p.save()
     return p
 
+def add_user(user):
+    try:
+        u = User.objects.get(username=user['email'])
+    except User.DoesNotExist:
+        u = User()
+    u.email = user['email']
+    u.username = user['email']
+    u.set_password(user['password'])
+    u.save()
+    try:
+        user_profile = UserProfile.objects.get(user__username=user['email'])
+    except UserProfile.DoesNotExist:
+        user_profile = UserProfile()
+    user_profile.user = u
+    user_profile.website = user['website']
+    user_profile.save()
+
 if __name__ == '__main__':
     print('Populating rango models....')
     populate()
+
