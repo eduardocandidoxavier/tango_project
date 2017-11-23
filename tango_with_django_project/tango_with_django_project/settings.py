01ed857c6261b1715a550b15200f86aeb7be1615
Eduardo Xavier <eduardo.xavier@gmail.com>
2017-11-23 14:48:40 -0200

    Set up for production on heroku

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -29,10 +29,11 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&1n7f+9$mu7-y-b^plu-prn&0y*^-^rqa*615xma(oyia3nj)2'
+#SECRET_KEY = '&1n7f+9$mu7-y-b^plu-prn&0y*^-^rqa*615xma(oyia3nj)2'
+SECRET_KEY = os.environ.get('SECRET_KEY', '&1n7f+9$mu7-y-b^plu-prn&0y*^-^rqa*615xma(oyia3nj)2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -144,3 +146,16 @@ EMAIL_HOST_PASSWORD = os.environ.get('MAIL_PASSWORD')
 EMAIL_PORT = 587
 
 
+#Settings for production
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
